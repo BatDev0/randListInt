b880e5e69eecce766643ea30d411fdcd69e9f281
BatDev <brodycritchlow@gmail.com>
2019-12-14 12:44:41 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+name="randList-Int-BatDev0", 
+    version="0.0.1",
+    author="Batdev0",
+    author_email="brody@tntsplash.net",
+    description="A small package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/batdev0/pythonRandomListInt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
